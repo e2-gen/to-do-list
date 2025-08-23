9ccabcddfecc99d841a452e3a525e5ae32b3b357
e2-gen <ebrahimjein@gmail.com>
2025-08-23 21:42:54 +0300

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -26,13 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 // إنشاء رابط تسجيل الدخول بجوجل
 require_once '../includes/google-config.php';
-$client = new Google_Client();
-$client->setClientId(GOOGLE_CLIENT_ID);
-$client->setClientSecret(GOOGLE_CLIENT_SECRET);
-$client->setRedirectUri(GOOGLE_REDIRECT_URI);
-$client->addScope('email');
-$client->addScope('profile');
-$googleLoginUrl = $client->createAuthUrl();
+$googleLoginUrl = getGoogleAuthUrl();
 ?>
 
 <!DOCTYPE html>
